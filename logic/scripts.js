6e706eda55f90855788d4e4c367231ef694e496e
Max <mishanin_max@mail.ru>
2016-10-01 11:48:36 +0300

    Initial commit

---
+++ logic/scripts.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Max on 01.10.2016.
+ */
+function addToTable() {
+    var str = document.getElementById("inputText").value;
+    confirm(str);
+    document.getElementById("btnA")
+}
+
