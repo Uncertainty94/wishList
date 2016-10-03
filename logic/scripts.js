f9f83d507844c8cf67968c482a3b944799be6ecf
mishanin <mishanin_max@mail.ru>
2016-10-03 16:21:33 +0300

    fix folders

--- logic/scripts.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Max on 01.10.2016.
- */
-function addToTable() {
-    var str = document.getElementById("inputText").value;
-    confirm(str);
-    document.getElementById("btnA")
-}
-
---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Max on 01.10.2016.
+ */
+function addToTable() {
+    confirm("Good");
+}
+
